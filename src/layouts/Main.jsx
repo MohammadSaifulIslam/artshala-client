bede87ec7eb2ffdb0a4fa4e20eb6e9e0ca045247
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-06-07 21:51:38 -0700

    footer component added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/NavBar/NavBar";
 
 const Main = () => {
   return (
     <div>
       <NavBar />
+      <ScrollRestoration/>
       <Outlet />
+      <Footer />
     </div>
   );
 };
