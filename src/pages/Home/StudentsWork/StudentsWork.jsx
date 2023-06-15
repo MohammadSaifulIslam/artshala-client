c79d434e050a82436a6c601ce179e3322f60a398
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-06-15 11:03:23 -0700

    student's work gallery layout changed

--- src/pages/Home/StudentsWork/StudentsWork.jsx
+++ src/pages/Home/StudentsWork/StudentsWork.jsx
@@ -1,7 +1,16 @@
-import workImg from '../../../assets/images/students-work/students-work.png';
 import ScrollRevealText from '../../../components/ScrollRevealText/ScrollRevealText';
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+// import styles
+import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lightgallery.css';
+
+
+// import plugins if you need
+
+
 const StudentsWork = () => {
+
     return (
         <section className="my-container mt-20">
             <SectionTitle title={"Student's Work"}></SectionTitle>
@@ -15,10 +24,31 @@ const StudentsWork = () => {
                         </p>
                     </ScrollRevealText>
                 </div>
-                <div className='col-span-2'>
-                    <img src={workImg} alt="Students work image" className='w-full h-full object-contain' />
+                <div className='col-span-2 grid grid-cols-4 gap-4' id='lightgallery'>
+
+                    <figure className='col-span-2 row-span-2 overflow-hidden cursor-pointer'>
+                        <img className='hover:scale-125 duration-300 w-full h-full object-cover overflow-hidden'  alt="img1" src="https://img.freepik.com/free-vector/hand-painted-watercolour-landscape-background-1603_1048-18535.jpg?size=626&ext=jpg&uid=R59828080&ga=GA1.2.562300066.1684249269&semt=sph" />
+                    </figure>
+                    <figure className='overflow-hidden cursor-pointer'>
+                        <img className='hover:scale-125 duration-300 overflow-hidden w-full h-full object-cover'  alt="img2" src="https://img.freepik.com/free-photo/vibrant-colors-explode-messy-watercolor-blob-generated-by-ai_188544-9791.jpg?size=626&ext=jpg&uid=R59828080&ga=GA1.2.562300066.1684249269&semt=sph" />
+                    </figure>
+                  
+                    <figure className='overflow-hidden cursor-pointer'>
+                        <img className='hover:scale-125 duration-300 w-full h-full object-cover overflow-hidden'  alt="img2" src="https://img.freepik.com/free-photo/vintage-landscape-with-gondolas_1160-162.jpg?size=626&ext=jpg&uid=R59828080&ga=GA1.2.562300066.1684249269&semt=sph" />
+                    </figure>
+                    <figure className='col-span-2 row-span-2 overflow-hidden cursor-pointer'>
+                        <img  className='hover:scale-125 duration-300 w-full h-full object-cover overflow-hidden'  alt="img1" src="https://img.freepik.com/free-vector/watercolor-chinese-style-background_52683-96100.jpg?size=626&ext=jpg&uid=R59828080&ga=GA1.2.562300066.1684249269&semt=sph" />
+                    </figure>
+                    <figure className='overflow-hidden cursor-pointer'>
+                        <img className='hover:scale-125 duration-300 w-full h-full object-cover overflow-hidden'  alt="img2" src="https://img.freepik.com/free-photo/top-view-attractive-woman-hands-drawing-amazing-picture-canvas-modern-cozy-art-workshop_574295-563.jpg?size=626&ext=jpg&uid=R59828080&ga=GA1.2.562300066.1684249269&semt=sph" />
+                    </figure>
+                    <figure className='overflow-hidden'>
+                        <img className='hover:scale-125 duration-300 w-full h-full object-cover overflow-hidden'  alt="img1" src="https://img.freepik.com/free-photo/dirty-coloured-palette-paint_23-2148660898.jpg?size=626&ext=jpg&uid=R59828080&ga=GA1.2.562300066.1684249269&semt=sph" />
+                    </figure>
+
                 </div>
             </div>
+
         </section>
     );
 };
